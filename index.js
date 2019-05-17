e4f135a0a85e15186faa638c8a1c42d826c07c8a
sofa5060 <47436802+sofa5060@users.noreply.github.com>
2019-05-17 21:46:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ searchForm.addEventListener('submit', e => {
   const searchLimit = document.getElementById('limit').value;
   // Get search
   const searchTerm = searchInput.value;
+  // Check for input
   if (searchTerm == '') {
+    // Show message
     showMessage('Please add a search term', 'alert-danger');
   }
+  // Clear field
   searchInput.value = '';
 
+  // Search Reddit
   reddit.search(searchTerm, searchLimit, sortBy).then(results => {
     let output = '<div class="card-columns">';
     console.log(results);
     results.forEach(post => {
+      // Check for image
       let image = post.preview
         ? post.preview.images[0].source.url
         : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
@@ -44,20 +49,30 @@ searchForm.addEventListener('submit', e => {
 
   e.preventDefault();
 });
-function showMessage(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert ${className}`;
 
+// Show Message Function
+function showMessage(message, className) {
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
   div.appendChild(document.createTextNode(message));
+  // Get parent
   const searchContainer = document.getElementById('search-container');
+  // Get form
   const search = document.getElementById('search');
+
+  // Insert alert
   searchContainer.insertBefore(div, search);
 
+  // Timeout after 3 sec
   setTimeout(function() {
     document.querySelector('.alert').remove();
   }, 3000);
 }
 
+// Truncate String Function
 function truncateString(myString, limit) {
   const shortened = myString.indexOf(' ', limit);
   if (shortened == -1) return myString;
